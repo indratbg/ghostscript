c0a3df91a3395dae2ddcd6621f1deab06429ed63
Indra Tobing <indra.tbg@gmail.com>
2019-08-01 07:25:33 +0700

    good

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 /**
  * Input file PDF harus di folder bin ghost script, untuk mengakali harus pake realpath
+ * Output file akan disimpan di recent directory
  */
 $output = [];
 exec("gswin64c -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -sOwnerPassword=ownerpass -sUserPassword=userpass -o output.pdf dummy.pdf", $output);
